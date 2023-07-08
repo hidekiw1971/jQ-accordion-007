56cfd3cec8fd12d9338dd54bf0ff6febfce25129
hideki watabe <hideki.w.1971@gmail.com>
2023-07-08 16:36:01 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
+  // setting
+
+  $(document).ready(function () {
+    $('.accordion-menu-item-btn:first').addClass('accordion-menu-item-btn-onLeft');
+    $('.accordion-menu-item-btn:first').addClass('accordion-menu-item-btn-onRight');
+    $('.accordion-menu-item-btn:first').siblings('ul').stop().slideDown();
+  });
+  $('.accordion-menu-item-btn').click(function () {
+    $(this).toggleClass('accordion-menu-item-btn-onLeft');
+    $(this).toggleClass('accordion-menu-item-btn-onRight');
+    $(this).siblings('ul').stop().slideToggle();
+  });
+
+  // /setting
+});
